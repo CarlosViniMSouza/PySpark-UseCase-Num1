a4fb375a283a8b86301207d2c4cc882aacd44ffe
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-02-07 23:03:45 -0300

    Steps 1 & 2 -> add

--- autogit.sh
+++ autogit.sh
@@ -1,7 +1,6 @@
-git init
 git add -A
-git commit -m "project started"
-git branch -M master
-git remote add origin https://github.com/CarlosViniMSouza/PySpark-UseCase-Num1.git
-git push -u origin master
+git commit -m "Steps 1 & 2 -> add"
+git push
 git status
+exit
+
---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+Step 1 - Installing PySpark: pip install pyspark
+
+Step 2 - Data Processing:
+"""
+
+from pyspark.sql import SparkSession
+import warnings
+
+# Ignore Warnings:
+warnings.filterwarnings("ignore")
